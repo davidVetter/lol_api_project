2fc29c594e69753ed4b1fff9e397a9dd52bac8c5
David Vetter <ddvetter23@gmail.com>
2023-03-27 19:30:49 -0500

    added code to return a single champ's full detailsto user endpoint

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -1,9 +1,29 @@
+const { readFile }= require('node:fs/promises');
+const { resolve } = require('node:path');
 const express = require('express');
 const router = express.Router();
+let contents = {}
+async function getTaric() {
+    try {
+        const filePath = resolve('../../codePlayground/lol_api_project/assets/dragontail-13.6.1/13.6.1/data/en_US/champion/Taric.json');
+        contents = await readFile(filePath, {encoding: 'utf8'});
+        console.log('this is contents: ', contents);
+    } catch (err) {
+        console.log('This is the error: ', err.message);
+    }
+}
+// let taric = require(`../../assets/dragontail-13.6.1/13.6.1/data/en_US/champion/Taric.json`);
+// fs.readFile(`../../assets/dragontail-13.6.1/13.6.1/data/en_US/champion/Taric.json`, 'utf-8', (err, data) => {
+//     if (err) throw err
 
-router.get('/', (req, res) => {
+//     taric = JSON.parse(data);
+// });
+
+
+router.get('/', async (req, res) => {
     console.log('In router GET /user');
-    res.send('Hello Posty');
+    await getTaric();
+    res.send(contents);
 })
 
 module.exports = router;
