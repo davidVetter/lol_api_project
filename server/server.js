bf9613ab672cb4e26a429b64d16ac44e3f29bb23
David Vetter <ddvetter23@gmail.com>
2023-03-31 12:54:09 -0500

    removed unneed cross domain code from server file

--- server/server.js
+++ server/server.js
@@ -2,14 +2,6 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', "*");
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-};
-
-app.use(allowCrossDomain);
 app.use(express.json());
 app.use(express.static('build'));
 
