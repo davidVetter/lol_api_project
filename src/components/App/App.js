a5ec989a541809a2f82fa7780815cd8fa16d77a5
David Vetter <ddvetter23@gmail.com>
2023-03-31 11:34:57 -0500

    added champ detail components to app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,13 +2,26 @@ import ChampTitle from '../ChampionDetails/ChampTitle/ChampTitle';
 import Header from '../Header/Header';
 import './App.css';
 import ChampLore from '../ChampionDetails/ChampLore/ChampLore';
+import ChampSkins from '../ChampionDetails/ChampSkins/ChampSkins';
+import AllyTips from '../ChampionDetails/ChampTips/AllyTips/AllyTips';
+import EnemyTips from '../ChampionDetails/ChampTips/EnemyTips/EnemyTips';
+import ChampTags from '../ChampionDetails/ChampTags/ChampTags';
+import ChampInfoRatings from '../ChampionDetails/ChampInfoRatings/ChampInfoRatings';
+import ChampStats from '../ChampionDetails/ChampStats/ChampStats';
+
 
 function App() {
   return (
     <div className="App">
       <Header />
       <ChampTitle />
+      <ChampSkins />
       <ChampLore />
+      <AllyTips />
+      <EnemyTips />
+      <ChampTags />
+      <ChampInfoRatings />
+      <ChampStats />
     </div>
   );
 }
