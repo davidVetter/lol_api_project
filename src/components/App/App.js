564e5659c0d0a9398e17703cb5a110f93e98c440
David Vetter <ddvetter23@gmail.com>
2023-03-31 11:24:12 -0500

    added ChampLore component boilerplate, added champ lore component to app.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,14 @@
 import ChampTitle from '../ChampionDetails/ChampTitle/ChampTitle';
 import Header from '../Header/Header';
 import './App.css';
+import ChampLore from '../ChampionDetails/ChampLore/ChampLore';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <ChampTitle />
+      <ChampLore />
     </div>
   );
 }
