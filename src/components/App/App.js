b3131af47e95d50ba80c2bcca0fc35d9d55866d3
David Vetter <ddvetter23@gmail.com>
2023-03-31 11:52:50 -0500

    added test axios get to a champ's info from server

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,20 +8,25 @@ import EnemyTips from '../ChampionDetails/ChampTips/EnemyTips/EnemyTips';
 import ChampTags from '../ChampionDetails/ChampTags/ChampTags';
 import ChampInfoRatings from '../ChampionDetails/ChampInfoRatings/ChampInfoRatings';
 import ChampStats from '../ChampionDetails/ChampStats/ChampStats';
-
+import axios from 'axios';
+import { useState } from 'react';
 
 function App() {
+  const [selectedChamp, setSelectedChamp] = useState('Taric');
+
+  const champInfo = axios.get(`/champ/${selectedChamp}`);
+
   return (
     <div className="App">
-      <Header />
-      <ChampTitle />
-      <ChampSkins />
-      <ChampLore />
-      <AllyTips />
-      <EnemyTips />
-      <ChampTags />
-      <ChampInfoRatings />
-      <ChampStats />
+      <Header champInfo={champInfo.data} />
+      <ChampTitle champInfo={champInfo.data} />
+      <ChampSkins champInfo={champInfo.data} />
+      <ChampLore champInfo={champInfo.data} />
+      <AllyTips champInfo={champInfo.data} />
+      <EnemyTips champInfo={champInfo.data} />
+      <ChampTags champInfo={champInfo.data} />
+      <ChampInfoRatings champInfo={champInfo.data} />
+      <ChampStats champInfo={champInfo.data} />
     </div>
   );
 }
