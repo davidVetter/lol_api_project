42b7c2f22ef7f3055896a6ea84877a8597942fc5
David Vetter <ddvetter23@gmail.com>
2023-03-31 10:24:15 -0500

    added ChampDetails folder, added ChampTitle to App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,3 +1,4 @@
+import ChampTitle from '../ChampionDetails/ChampTitle/ChampTitle';
 import Header from '../Header/Header';
 import './App.css';
 
@@ -5,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <ChampTitle />
     </div>
   );
 }
