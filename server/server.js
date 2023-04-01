f94df62e8a1421ae5a42090e358c2df9561bcb62
David Vetter <ddvetter23@gmail.com>
2023-04-01 10:26:58 -0500

    setup server to server champion imgs from data dragon directory

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
+const path = require('path');
 
 app.use(express.json());
 app.use(express.static('build'));
+app.use('/img', express.static(path.join(__dirname, '../assets/dragontail-13.6.1/img/champion/')));
 
 const champRouter = require('./routes/champ.router');
 app.use('/champ', champRouter);
