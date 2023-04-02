8c2c980d3efbc0bf1047e3eae7d210529debe250
David Vetter <ddvetter23@gmail.com>
2023-04-01 21:33:19 -0500

    added spell static route to server

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const path = require('path');
 app.use(express.json());
 app.use(express.static('build'));
 app.use('/img', express.static(path.join(__dirname, '../assets/dragontail-13.6.1/img/champion/')));
+app.use('/spell', express.static(path.join(__dirname, '../assets/dragontail-13.6.1/13.6.1/img/spell/')));
 
 const champRouter = require('./routes/champ.router');
 app.use('/champ', champRouter);
