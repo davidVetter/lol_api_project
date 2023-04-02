4fb5ebc89cb79a948e6f21b1ef5f25f547220e50
David Vetter <ddvetter23@gmail.com>
2023-04-01 21:30:49 -0500

    added display of champ abilites in ChampAbilities component

--- src/components/ChampionDetails/ChampAbilities/ChampAbilites.jsx
+++ src/components/ChampionDetails/ChampAbilities/ChampAbilites.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React from "react";
 
-export default function ChampAbilites() {
+export default function ChampAbilites({ champInfo, selectedChamp }) {
   return (
-    <div>ChampAbilites</div>
-  )
+    <div>
+      {champInfo[selectedChamp].spells.map((spell) => {
+         return <div key={spell.id}>
+            <div>
+                {/* <img alt={selectedChamp} src={}/> */}
+                <span style={{fontWeight: 'bolder', textDecoration: "underline", width: '100%', textAlign: 'left'}}>{spell.name}</span>
+                <div>&nbsp;{spell.description}</div>
+            </div>
+         </div>;
+      })}
+    </div>
+  );
 }
