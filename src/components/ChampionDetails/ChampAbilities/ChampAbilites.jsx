3d2b8eb3d740756861d7dee93d6d4302e11d1c99
David Vetter <ddvetter23@gmail.com>
2023-04-01 21:41:18 -0500

    added ability image to ability listing

--- src/components/ChampionDetails/ChampAbilities/ChampAbilites.jsx
+++ src/components/ChampionDetails/ChampAbilities/ChampAbilites.jsx
@@ -4,13 +4,26 @@ export default function ChampAbilites({ champInfo, selectedChamp }) {
   return (
     <div>
       {champInfo[selectedChamp].spells.map((spell) => {
-         return <div key={spell.id}>
+        return (
+          <div key={spell.id}>
             <div>
-                {/* <img alt={selectedChamp} src={}/> */}
-                <span style={{fontWeight: 'bolder', textDecoration: "underline", width: '100%', textAlign: 'left'}}>{spell.name}</span>
-                <div>&nbsp;{spell.description}</div>
+              <span
+                style={{
+                  fontWeight: "bolder",
+                  textDecoration: "underline",
+                  width: "100%",
+                  textAlign: "left",
+                }}
+              >
+                {spell.name}
+              </span>
+              <div>
+                <img alt={selectedChamp} src={`/spell/${spell.id}.png`} />
+              </div>
+              <div>&nbsp;{spell.description}</div>
             </div>
-         </div>;
+          </div>
+        );
       })}
     </div>
   );
