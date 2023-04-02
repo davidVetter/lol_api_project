6bf173bef5cbbc1786963e3d2a4f3c4ddb79cd8a
David Vetter <ddvetter23@gmail.com>
2023-04-02 11:44:05 -0500

    added html-react-parse to ChampP component, some passives have html formatting like q,w,e,r abilities

--- src/components/ChampionDetails/ChampAbilities/ChampP/ChampP.jsx
+++ src/components/ChampionDetails/ChampAbilities/ChampP/ChampP.jsx
@@ -1,11 +1,11 @@
-import React from "react";
+import parse from "html-react-parser";
 
 export default function ChampP({ champInfo, selectedChamp }) {
   return (
     <>
       <div style={{fontWeight: 'bolder', textDecoration: 'underline'}}>{`${champInfo[selectedChamp].passive.name} (P)`}</div>
       <img alt={selectedChamp} src={`/passive/${champInfo[selectedChamp].passive.image.full}`} />
-      <div>{champInfo[selectedChamp].passive.description}</div>
+      {parse(`<div>${champInfo[selectedChamp].passive.description}</div>`)}
     </>
   );
 }
