fd8997094cf5cfd36647fe87829afb4ca87b3d9c
David Vetter <ddvetter23@gmail.com>
2023-04-02 11:46:08 -0500

    removed some unused folders and files for champion abilities

--- src/components/ChampionDetails/ChampAbilities/ChampE/ChampE.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function ChampE() {
-  return (
-    <div>ChampE</div>
-  )
-}
--- src/components/ChampionDetails/ChampAbilities/ChampW/ChampW.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function ChampW() {
-  return (
-    <div>ChampW</div>
-  )
-}
