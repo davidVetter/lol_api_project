a1d12b36215a6708e1cdbe3fbe394a8a62eed3c5
David Vetter <ddvetter23@gmail.com>
2023-04-03 15:41:47 -0500

    added some basic styling to SingleSkin component

--- src/components/ChampionDetails/ChampSkins/SingleSkinCard/SingleSkinCard.jsx
+++ src/components/ChampionDetails/ChampSkins/SingleSkinCard/SingleSkinCard.jsx
@@ -3,19 +3,27 @@ export default function SingleSkinCard({
   selectedChampion,
   setCurrentBackgroundImage,
 }) {
+  const styling = {
+    width: '90%',
+    backgroundColor: 'tan',
+    padding: '10px',
+    marginBottom: '10px',
+    borderRadius: '7px'    
+  }
   return (
     <div id="test"
       onClick={() => {
         console.log('click');
         setCurrentBackgroundImage(`/img/centered/${selectedChampion}_${skin.num}.jpg`);
       }}
+      style={styling}
     >
       <img
         alt={`Skin of ${selectedChampion}`}
-        style={{ width: "90%" }}
+        style={{width: '100%', borderRadius: '7px'}}
         src={`/img/splash/${selectedChampion}_${skin.num}.jpg`}
       />
-      <div>{skin.name === "default" ? selectedChampion : skin.name}</div>
+      <div style={{paddingTop: '5px', fontWeight: 'bolder'}}>{skin.name === "default" ? selectedChampion : skin.name}</div>
     </div>
   );
 }
