5918d160363a39e6ddf412c56cbb0dff9d3f96f9
David Vetter <ddvetter23@gmail.com>
2023-04-06 10:00:24 -0500

    added title and some styling to ChampAbilities component

--- src/components/ChampionDetails/ChampAbilities/ChampAbilites.jsx
+++ src/components/ChampionDetails/ChampAbilities/ChampAbilites.jsx
@@ -1,22 +1,20 @@
 import ChampP from "../ChampAbilities/ChampP/ChampP";
 import SingleAbility from "./SingleAbility/SingleAbility";
+import { themeColors } from "../../../Theme/ThemeColors";
 
 export default function ChampAbilites({ champInfo, selectedChamp }) {
-//   function convertChampIdToFourDigits(champId) {
-//     let fourDigitID = '';
-//     if (champId.length === 1){
-//         fourDigitID = `000${champId}`;
-//     } else if (champId.length === 2){
-//         fourDigitID = `00${champId}`;
-//     } else if (champId.length === 3) {
-//         fourDigitID = `0${champId}`;
-//     } else {
-//         fourDigitID = champId;
-//     }
-//     return fourDigitID;
-//   }
-    return (
-    <div>
+  const abilitiesStyling = {
+    backgroundColor: themeColors.main,
+    borderRadius: "7px",
+    width: "90%",
+    margin: "10px",
+  };
+
+  return (
+    <div style={abilitiesStyling}>
+      <h2 style={{ width: "100%", textAlign: "left", marginLeft: "10px" }}>
+        Abilites
+      </h2>
       <ChampP champInfo={champInfo} selectedChamp={selectedChamp} />
       {champInfo[selectedChamp].spells.map((spell, index) => {
         return (
