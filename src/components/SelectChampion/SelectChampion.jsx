679648f6e65688d30d51fb100c0566db2f352dfe
David Vetter <ddvetter23@gmail.com>
2023-04-10 16:35:35 -0500

    ran code formatter on champion list

--- src/components/SelectChampion/SelectChampion.jsx
+++ src/components/SelectChampion/SelectChampion.jsx
@@ -1,23 +1,28 @@
-import Select from 'react-select';
+import Select from "react-select";
 
 export default function SelectChampion({
   setSelectedChamp,
   setChampInfo,
   champList,
 }) {
-
-  const options = Object.entries(champList).map(([key, value], index) => (
-    {value: value, label: <div><img alt={`${value} from League of Legends`}
-    style={{ width: "90%" }}
-    src={`/icon/${value}.png`}/>{value}</div>}
-  ))
+  const options = Object.entries(champList).map(([key, value], index) => ({
+    value: value,
+    label: (
+      <div>
+        <img
+          alt={`${value} from League of Legends`}
+          style={{ width: "90%" }}
+          src={`/icon/${value}.png`}
+        />
+        {value}
+      </div>
+    ),
+  }));
 
   function changeChamp(e) {
     setSelectedChamp(e.value);
     setChampInfo({});
   }
 
-  return (
-    <Select options={options} onChange={(e) => changeChamp(e)}/>
-  );
+  return <Select options={options} onChange={(e) => changeChamp(e)} />;
 }
